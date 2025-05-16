f395c0c217d5c69e3fb65d9ecbbccde8f8ababa7
Marcelo Henrique Fonseca <6546651+marcelohfonseca@users.noreply.github.com>
2025-05-15 21:49:23 -0300

    aula 03: crud

--- tests/test_app.py
+++ tests/test_app.py
@@ -15,7 +15,65 @@ def test_root_deve_retornar_ola_mundo():
     # arrange
     client = TestClient(app)
     # act
-    response = client.get("/")
+    response = client.get('/')
     # assert
-    assert response.json() == {"message": "Hello World!"}
+    assert response.json() == {'message': 'Hello World!'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'email': 'alice@example.com',
+        'username': 'alice',
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'id': 1,
+                'email': 'alice@example.com',
+                'username': 'alice',
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1/',
+        json={
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'secret',
+        },
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'email': 'bob@example.com',
+        'username': 'bob',
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'email': 'bob@example.com',
+        'username': 'bob',
+    }
